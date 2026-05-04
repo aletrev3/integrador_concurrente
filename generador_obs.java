4d72ded4ed3a1817376939f48a6279436b22b49e
nlopeez24 <nayelizarco77@gmail.com>
2026-05-03 23:19:04 -0600

    Update generador_obs.java
    Habia errores de GitHub <<<>>> head ====

--- generador_obs.java
+++ generador_obs.java
@@ -1,8 +1,7 @@
 
-import java.util.Random;
+import java.util.*;
 
 public class generador_obs {
-<<<<<<< HEAD
     private interfaz ventana;
     private Map<Integer, obstaculos> mapa = new HashMap<>();
 
@@ -10,68 +9,55 @@ public class generador_obs {
         this.ventana = ventana;
     }
 
-    // Nayeli
-=======
->>>>>>> 548e64b3d29ef02731b9deb520fb199264864afe
     public void generar() {
-        String[] tipos = {"choque", "bache", "manifestacion", "construccion"};
+        String[] tipos = { "choque", "bache", "manifestacion", "construccion" };
         Random random = new Random();
         int contadorId = 1;
 
-<<<<<<< HEAD
+
+        
         while (!sistema.isJuegoTerminado()) {
-
-=======
-        while (true) {
->>>>>>> 548e64b3d29ef02731b9deb520fb199264864afe
             String tipo = tipos[random.nextInt(tipos.length)];
-            int obs_id = contadorId;
-            String obs_tipo = tipo;
-            int obs_consumo = random.nextInt(100) + 1;
 
-<<<<<<< HEAD
             int consumo = random.nextInt(15) + 5;
-
-            if (tipo.equals("manifestacion")) {
+            if (tipo.equals("manifestacion"))
                 consumo = 30;
-            }
-            obstaculos obs = new obstaculos(
-                    contadorId,
-                    tipo,
-                    consumo);
 
+            obstaculos obs = new obstaculos(contadorId, tipo, consumo);
             mapa.put(contadorId, obs);
 
             Thread hilo = new Thread(obs);
             hilo.start();
 
-            ventana.agregarBoton(
-                    contadorId,
-                    tipo);
+            ventana.agregarBoton(contadorId, tipo);
 
             contadorId++;
 
             try {
-                Thread.sleep(4000);
+                
+                Thread.sleep(2000);
             } catch (Exception e) {
-                e.printStackTrace();
+                e.printStackTrace(); 
             }
         }
 
         System.out.println("Generador de obstáculos detenido.");
-=======
-            obstaculos obs_1 = new obstaculos(obs_id, obs_tipo, obs_consumo);
+    }
 
-            Thread hilo = new Thread(obs_1);
-            hilo.start();
+    public void matarPorId(int id) {
+        obstaculos obs = mapa.get(id);
 
-            contadorId++;
-                    try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (obs == null) {
+            ventana.mostrar("No existe ese ID");
+            return;
         }
+
+        if (!obs.estaActivo()) {
+            ventana.mostrar("Ya estaba muerto");
+            return;
         }
->>>>>>> 548e64b3d29ef02731b9deb520fb199264864afe
+
+        obs.detener();
+        ventana.marcarMuerto(id);
     }
 }
